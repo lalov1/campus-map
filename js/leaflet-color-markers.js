45c740017f607c6adf5efd7fe883717aa8aa23d9
Lallo Vigil <lallo.vigil@gmail.com>
2017-06-08 22:30:02 -0600

    Pushing everything

---
+++ js/leaflet-color-markers.js
@@ -0,0 +1,107 @@
+var orangeCSUP = new L.Icon({
+	iconUrl: 'img/markers/orange-csup-icon.png',
+	shadowUrl: 'img/markers/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var redCSUP = new L.Icon({
+	iconUrl: 'img/markers/red-csup-icon.png',
+	shadowUrl: 'img/markers/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var blueCSUP = new L.Icon({
+	iconUrl: 'img/markers/blue-csup-icon.png',
+	shadowUrl: 'img/markers/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var greenCSUP = new L.Icon({
+	iconUrl: 'img/markers/green-csup-icon.png',
+	shadowUrl: 'img/markers/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var blueIcon = new L.Icon({
+	iconUrl: 'img/markers/marker-icon-2x-blue.png',
+	shadowUrl: 'img/markers/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var redIcon = new L.Icon({
+	iconUrl: 'img/markers/marker-icon-2x-red.png',
+	shadowUrl: 'img/markers/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var greenIcon = new L.Icon({
+	iconUrl: 'img/markers/marker-icon-2x-green.png',
+	shadowUrl: 'img/markers/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var orangeIcon = new L.Icon({
+	iconUrl: 'img/markers/marker-icon-2x-orange.png',
+	shadowUrl: 'img/markers/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var yellowIcon = new L.Icon({
+	iconUrl: 'img/markers/marker-icon-2x-yellow.png',
+	shadowUrl: 'img/markers/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var violetIcon = new L.Icon({
+	iconUrl: 'img/markers/marker-icon-2x-violet.png',
+	shadowUrl: 'img/markers/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var greyIcon = new L.Icon({
+	iconUrl: 'img/markers/marker-icon-2x-grey.png',
+	shadowUrl: 'img/markers/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var blackIcon = new L.Icon({
+	iconUrl: 'img/markers/marker-icon-2x-black.png',
+	shadowUrl: 'img/markers/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
